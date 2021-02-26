99bdcf1b2e8f1ef4dad1626e7174d8f38b3e2758
Jeff Lee <jefflee@Jeffui-MacBookPro.local>
2021-02-26 10:07:12 +0900

    마지막 페이지까지 넘어가는 것 구현. 검색어는 아직 한번에 한개밖에 지정 못함.

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from urllib.parse import urlparse
 from urllib import parse 
 
+tempstorename = ' '
+
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')    # 웹 브라우저를 띄우지 않는 headless chrome 옵션 적용
 options.add_argument('disable-gpu')    # GPU 사용 안함
@@ -33,59 +35,82 @@ searchbox.send_keys(Keys.ENTER)
 
 time.sleep(5) 
 
-driver.switch_to.frame(driver.find_element_by_id('searchIframe'))
-#stores = driver.find_element_by_id('_pcmap_list_scroll_container')
-stores = driver.find_elements_by_css_selector('#_pcmap_list_scroll_container > ul > li')
-
-for p in stores: 
-   name = p.find_element_by_tag_name('span')
-   name.click()
-
-   #장소 상세 설명 창으로 가주기 
-   driver.switch_to.default_content()
-   driver.switch_to.frame(driver.find_element_by_id('entryIframe'))
-   
-   time.sleep(2)
-
-   detail = driver.find_element_by_class_name('place_detail_wrapper')
-   intro1 = detail.find_element_by_id('_title') #이름 + 종목명 
-   intro2 = intro1.find_elements_by_tag_name('span')
-   
-   print("장소 이름: "+intro2[0].text)
-   print("장소 종목명: "+intro2[1].text)
-
-   detail2 = driver.find_element_by_xpath('//*[@id="app-root"]/div/div[2]/div[1]/div/div/div[1]/div')
-   finalinfo = detail2.find_elements_by_tag_name('em')
-   print("장소 별점: "+finalinfo[0].text)
-   print("장소 방문자리뷰 수: "+finalinfo[1].text)
-   print("장소 블로그리뷰 수: "+finalinfo[2].text)
-
-   time.sleep(4)
-
-   driver.find_element_by_class_name('place_fixed_maintab').find_elements_by_tag_name('a')[0].click()
-
-   address = driver.find_elements_by_tag_name('ul')[0].find_elements_by_tag_name('li')[1].find_elements_by_tag_name('span')[1].text
-   print("장소 주소: "+address)
-
-   #원래 프레임으로 복귀해주기 
-   driver.switch_to.default_content()
-   
-   #print("현재 url: "+driver.current_url) 
-   url = parse.urlparse(driver.current_url)
-
-   complexcoordinate = parse.parse_qs(url.query)['c'][0]
-   simplecoordinate = complexcoordinate.split(',')
-   #print(simplecoordinate)
-
-   print(float(simplecoordinate[0]))
-   print(float(simplecoordinate[1]))
-   print() 
-
+### 여기서부터 반복문 시작 
+while True: 
    driver.switch_to.frame(driver.find_element_by_id('searchIframe'))
 
-   time.sleep(4)
-   
+   #무한 스크롤 
+   eula = driver.find_element_by_id('_pcmap_list_scroll_container')
+   while True: 
+      prev_sh = driver.execute_script('return arguments[0].scrollHeight', eula)
+      driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', eula)
+      time.sleep(10) 
+      current_sh = driver.execute_script('return arguments[0].scrollHeight', eula)
+      if prev_sh == current_sh:
+         break
 
-#driver.close()
+   stores = driver.find_elements_by_css_selector('#_pcmap_list_scroll_container > ul > li')
+
+   time.sleep(10) 
+
+   if stores[0].find_elements_by_tag_name('div')[1].find_element_by_tag_name('span').text == tempstorename: #마지막 페이지에 도달함 
+      break 
+
+   tempstorename = stores[0].find_elements_by_tag_name('div')[1].find_element_by_tag_name('span').text #마지막 페이지 아니면 다시 맨 처음 가게 이름 저장해놓음 
+
+   for p in stores: 
+
+      #개별 장소 상세 설명 클릭하기 
+
+      p.find_elements_by_tag_name('div')[1].find_element_by_tag_name('span').click()
+      time.sleep(5)
+
+      #장소 상세 설명 창으로 가주기 
+      driver.switch_to.default_content()
+      driver.switch_to.frame(driver.find_element_by_id('entryIframe'))
+      
+      #이름, 종목명 가져오기 
+      detail = driver.find_element_by_class_name('place_detail_wrapper')
+      intro1 = detail.find_element_by_id('_title') #이름 + 종목명 
+      intro2 = intro1.find_elements_by_tag_name('span')
+      print("장소 이름: "+intro2[0].text)
+      print("장소 종목명: "+intro2[1].text)
+
+      
+      #별점, 방문자 리뷰, 블로그 리뷰 가져오기 
+      detail2 = driver.find_element_by_xpath('//*[@id="app-root"]/div/div[2]/div[1]/div/div/div[1]/div')
+      finalinfo = detail2.find_elements_by_tag_name('span') #'별점'이 적힌 독립적인 span 이 하나 존재한다! 
+
+      for k in finalinfo: 
+         #print(k.text)
+         if k.text.find("별점") != -1 and k.text.find("/5") != -1:
+            print("장소 별점: "+k.find_element_by_tag_name('em').text)
+         if k.text.find("방문자리뷰") != -1:
+            print("장소 방문자리뷰 수: "+k.find_element_by_tag_name('em').text)
+         if k.text.find("블로그리뷰") != -1:
+            print("장소 블로그리뷰 수: "+k.find_element_by_tag_name('em').text)
+         if k.text.find("주문자리뷰") != -1:
+            print("장소 주문자리뷰 수: "+k.find_element_by_tag_name('em').text)
+      
+
+      #주소 가져오기 
+      address_list = driver.find_elements_by_tag_name('li')
+      for x in address_list: 
+         if x.text.find("주소") != -1:
+            print("주소: "+x.find_elements_by_tag_name('span')[1].text)
+            print() 
+            break 
+
+
+      driver.switch_to.default_content()
+      driver.switch_to.frame(driver.find_element_by_id('searchIframe'))
+
+      time.sleep(5)
+
+   driver.find_element_by_xpath('//*[@id="app-root"]/div/div/div[2]').find_elements_by_tag_name('svg')[1].click() #'다음페이지' 클릭하는 코드 
+   driver.switch_to.default_content()
+
+
+
 
 
